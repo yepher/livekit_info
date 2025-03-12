27f5c937195dc64266e46d243fcc063eb50f90c0
Chris Wilson <github@yepher.com>
2025-03-12 10:38:53 -0500

    add required vad for openai

--- to_fix/openai_test.py
+++ to_fix/openai_test.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from livekit.agents import JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice import AgentTask, VoiceAgent
 from livekit.agents.voice.room_io import RoomInputOptions
-from livekit.plugins import cartesia, deepgram, openai
+from livekit.plugins import cartesia, deepgram, openai, silero
 
 logger = logging.getLogger("roomio-example")
 logger.setLevel(logging.INFO)
@@ -22,24 +22,8 @@ class AlloyTask(AgentTask):
             stt=openai.STT(),
             llm=openai.LLM(model="gpt-4o-mini"),
             tts=openai.TTS(),
+            vad=silero.VAD.load(),
         )
-
-    async def on_enter(self) -> None:
-        """Called when the task is entered"""
-        logger.info("on_enter")
-
-    async def on_exit(self) -> None:
-        """Called when the task is exited"""
-        logger.info("on_exit")
-
-    async def on_end_of_turn(self, chat_ctx: llm.ChatContext, new_message: llm.ChatMessage) -> None:
-        """Called when the user has finished speaking, and the LLM is about to respond
-
-        This is a good opportunity to update the chat context or edit the new message before it is
-        sent to the LLM.
-        """
-        logger.info(f"on_end_of_turn: cat_ctx={chat_ctx}; new_message={new_message}")
-
 
 
 async def entrypoint(ctx: JobContext):
